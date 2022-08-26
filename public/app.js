96d062800be9594bdc300ca333e7499543162d55
shree <sammmmmu123@gmail.com>
2022-08-26 14:36:07 +0530

    drew a bar graph

--- public/app.js
+++ public/app.js
@@ -1,80 +1,31 @@
-
-(async function () {
-    const res = await fetch('/api/v1/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({
-            "company": "apple",
-            "frm": "2022-02-01",
-            "to": "2022-04-10"
+fetch('/api/v1', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({
+        company: 'apple'
+    })
+}).then((data) => data.json()).then((vals) => {
+    const svgWidth = 700, svgHeight = 200;
+    //vals.data = vals.data.slice(200);
+    const barWidth = svgWidth / vals.data.length;
+    console.log(barWidth);
+    const svgele = d3.select('#bar-chart')
+        .attr('width', svgWidth)
+        .attr('height', svgHeight)
+        .selectAll('rect')
+        .data(vals.data)
+        .enter()
+        .append('rect')
+        .attr('width', barWidth)
+        .attr('height', function (d) {
+            return d.Value;
         })
-    });
-    const resdata = await res.json();
-
-    // set the dimensions and margins of the graph
-    var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-    // append the svg object to the body of the page
-    var svg = d3.select("#gpo")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-    //Read the data
-    d3.json(resdata.data,
-        // When reading the csv, I must format variables:
-        function (d) {
-            return { date: d3.timeParse("%Y-%m-%d")(d.Date), value: d.High };
-        }, function (d) {
-            console.log(d);
-        },
-
-        // Now I can use this dataset:
-        function (data) {
-            console.log(data);
-            // Add X axis --> it is a date format
-            var x = d3.scaleTime()
-                .domain(d3.extent(data, function (d) { return d.date; }))
-                .range([0, width]);
-            svg.append("g")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-
-            // Add Y axis
-            var y = d3.scaleLinear()
-                .domain([0, d3.max(data, function (d) { return +d.value; })])
-                .range([height, 0]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
-
-            // Add the line
-            svg.append("path")
-                .datum(data)
-                .attr("fill", "none")
-                .attr("stroke", "steelblue")
-                .attr("stroke-width", 1.5)
-                .attr("d", d3.line()
-                    .x(function (d) { return x(d.date); })
-                    .y(function (d) { return y(d.value); })
-                );
-
+        .attr('y', function (d) {
+            return svgHeight - d.Value;
+        })
+        .attr('x', function (d, i) {
+            return (barWidth + 4) * i;
         });
-
-})();
-async function run() {
-    var svg = d3.select("#gpo")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
-
-}
+});
